ed4b6119650b32101f4602df3741a9a83b743ee2
Ana MC <troglodita91@gmail.com>
2018-01-17 13:39:19 +0000

    Actualización

--- author.php
+++ author.php
@@ -9,8 +9,19 @@
 <div class="row">
     
         <div class="col-lg-8 col-sm-8 col-xs-12 text-lg-left text-center">
-            
-        <img alt="Imagen por defecto del autor " src="<?php echo get_template_directory_uri();?>/img/makeup-default.png"> <!--Comprobar si el autor tiene foto, si no, poner por defect-->
+        
+        <!--Comprobar si el autor tiene foto, si no, poner por defect-->
+        <img alt="Imagen por defecto del autor " src="<?php if(file_exists( get_template_directory_uri().'/img/'.get_the_author_meta('nickname'))){
+                    echo get_template_directory_uri().'/img/'.get_the_author_meta('nickname');
+                    }else{echo get_template_directory_uri().'/img/makeup-default.png';} ?>"> 
+        
+         <?php 
+          if(has_post_thumbnail() ) {
+            $postImg = get_the_post_thumbnail_url();
+          } else{
+            $postImg = get_template_directory_uri()."/img/image-defecto.jpg";
+          }
+        ?>
         
         </div>
 
